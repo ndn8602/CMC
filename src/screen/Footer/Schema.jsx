f1fbffe7a1735282f5ad4e641bed9649cf968e4c
Nham <nguyenducnhamndn@gmail.com>
2022-10-13 23:22:21 +0700

    cmc

--- src/screen/Footer/Schema.jsx
+++ src/screen/Footer/Schema.jsx
@@ -4,7 +4,13 @@ const emailRules =
   /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 export const contactFormSchema = yup.object().shape({
-  name: yup.string().required("Required").min(2, "Nhap do"),
-  email: yup.string().required("Required").matches(emailRules, "Sai Form roi"),
-  phone: yup.string().required("Required").length(10, "du 10 so thi do"),
+  name: yup
+    .string()
+    .required("Please input your full name")
+    .min(2, "Please input your full name"),
+  email: yup
+    .string()
+    .required("Required")
+    .matches(emailRules, "Please check your email"),
+  phone: yup.number().required("Please input your phone number."),
 });
