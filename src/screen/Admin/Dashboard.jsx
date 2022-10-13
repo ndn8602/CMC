96a2b54f8c2979b419aebe19bff7f5c1830e5ec8
Nham <nguyenducnhamndn@gmail.com>
2022-10-13 18:06:15 +0700

    cmc admin

--- src/screen/Admin/Dashboard.jsx
+++ src/screen/Admin/Dashboard.jsx
@@ -1,20 +1,11 @@
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Offcanvas from "react-bootstrap/Offcanvas";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/AuthContext";
 import "./Dashboard.css";
 const Dashboard = () => {
   const navigate = useNavigate();
   const { user, logout } = UserAuth();
-  const [show, setShow] = useState(true);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const handleLogout = async () => {
     try {
@@ -26,80 +17,272 @@ const Dashboard = () => {
     }
   };
   return (
-    <Row className="dashboard m-0">
-      <Col md={2} className="sidebar">
-        <div className="dashboard-title">ADMIN</div>
-        <div className="dashboard-info">
-          <img src="./image/Logo.png" className="dashboard-avatar" alt="" />
-          <span>{user && user.email}</span>
+    <>
+      <div className="wrapper">
+        <div className="preloader flex-column justify-content-center align-items-center">
+          <img
+            className="animation__shake"
+            src="dist/img/AdminLTELogo.png"
+            alt="AdminLTELogo"
+            height={60}
+            width={60}
+          />
         </div>
-        <Link to="./content">
-          <Button variant="warning" className="w-100 mb-2">
-            Content
-          </Button>
-        </Link>
-        <Link to="./contact">
-          <Button variant="primary" className="w-100 mb-2">
-            Contact
-          </Button>
-        </Link>
-        <Button variant="primary" onClick={handleShow} className="w-100">
-          Launch
-        </Button>
-
-        <Offcanvas show={show} onHide={handleClose}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title>
-              <div className="admin-brand">Dashboard</div>
-              <div className="admin-avartar">
-                {/* <img src="./image/Logo.png" alt="" /> */}
-              </div>
-              <div className="admin-name">User:{user && user.email}</div>
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Link to="./content">
-              <Button variant="warning" className="w-100">
-                Content
-              </Button>
-            </Link>
-            <Link to="./contact">
-              <Button variant="primary" className="w-100">
+        <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                data-widget="pushmenu"
+                href="#!"
+                role="button"
+              >
+                <i className="fas fa-bars" />
+              </a>
+            </li>
+            <li className="nav-item d-none d-sm-inline-block">
+              <a href="#!" className="nav-link">
                 Contact
-              </Button>
-            </Link>
-            <Button variant="warning" onClick={handleLogout}>
-              Logout
-            </Button>
-          </Offcanvas.Body>
-        </Offcanvas>
-      </Col>
-      <Col md={10} className="p-0">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto justify-content-end">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </Col>
-    </Row>
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                data-widget="navbar-search"
+                href="#!"
+                role="button"
+              >
+                <i className="fas fa-search" />
+              </a>
+              <div className="navbar-search-block">
+                <form className="form-inline">
+                  <div className="input-group input-group-sm">
+                    <input
+                      className="form-control form-control-navbar"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                    />
+                    <div className="input-group-append">
+                      <button className="btn btn-navbar" type="submit">
+                        <i className="fas fa-search" />
+                      </button>
+                      <button
+                        className="btn btn-navbar"
+                        type="button"
+                        data-widget="navbar-search"
+                      >
+                        <i className="fas fa-times" />
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link" data-toggle="dropdown" href="#!">
+                <i className="far fa-comments" />
+                <span className="badge badge-danger navbar-badge">3</span>
+              </a>
+              <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <a href="#!" className="dropdown-item">
+                  <div className="media">
+                    <img
+                      src="dist/img/user1-128x128.jpg"
+                      alt="User Avatar"
+                      className="img-size-50 mr-3 img-circle"
+                    />
+                    <div className="media-body">
+                      <h3 className="dropdown-item-title">
+                        Brad Diesel
+                        <span className="float-right text-sm text-danger">
+                          <i className="fas fa-star" />
+                        </span>
+                      </h3>
+                      <p className="text-sm">Call me whenever you can...</p>
+                      <p className="text-sm text-muted">
+                        <i className="far fa-clock mr-1" /> 4 Hours Ago
+                      </p>
+                    </div>
+                  </div>
+                </a>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item">
+                  <div className="media">
+                    <img
+                      src="dist/img/user8-128x128.jpg"
+                      alt="User Avatar"
+                      className="img-size-50 img-circle mr-3"
+                    />
+                    <div className="media-body">
+                      <h3 className="dropdown-item-title">
+                        John Pierce
+                        <span className="float-right text-sm text-muted">
+                          <i className="fas fa-star" />
+                        </span>
+                      </h3>
+                      <p className="text-sm">I got your message bro</p>
+                      <p className="text-sm text-muted">
+                        <i className="far fa-clock mr-1" /> 4 Hours Ago
+                      </p>
+                    </div>
+                  </div>
+                </a>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item">
+                  <div className="media">
+                    <img
+                      src="dist/img/user3-128x128.jpg"
+                      alt="User Avatar"
+                      className="img-size-50 img-circle mr-3"
+                    />
+                    <div className="media-body">
+                      <h3 className="dropdown-item-title">
+                        Nora Silvester
+                        <span className="float-right text-sm text-warning">
+                          <i className="fas fa-star" />
+                        </span>
+                      </h3>
+                      <p className="text-sm">The subject goes here</p>
+                      <p className="text-sm text-muted">
+                        <i className="far fa-clock mr-1" /> 4 Hours Ago
+                      </p>
+                    </div>
+                  </div>
+                </a>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item dropdown-footer">
+                  See All Messages
+                </a>
+              </div>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link" data-toggle="dropdown" href="#!">
+                <i className="far fa-bell" />
+                <span className="badge badge-warning navbar-badge">15</span>
+              </a>
+              <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span className="dropdown-item dropdown-header">
+                  15 Notifications
+                </span>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item">
+                  <i className="fas fa-envelope mr-2" /> 4 new messages
+                  <span className="float-right text-muted text-sm">3 mins</span>
+                </a>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item">
+                  <i className="fas fa-users mr-2" /> 8 friend requests
+                  <span className="float-right text-muted text-sm">
+                    12 hours
+                  </span>
+                </a>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item">
+                  <i className="fas fa-file mr-2" /> 3 new reports
+                  <span className="float-right text-muted text-sm">2 days</span>
+                </a>
+                <div className="dropdown-divider" />
+                <a href="#!" className="dropdown-item dropdown-footer">
+                  See All Notifications
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                data-widget="fullscreen"
+                href="#!"
+                role="button"
+              >
+                <i className="fas fa-expand-arrows-alt" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                data-widget="control-sidebar"
+                data-controlsidebar-slide="true"
+                href="#!"
+                role="button"
+              >
+                <i className="fas fa-th-large" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <aside className="main-sidebar sidebar-dark-primary elevation-4">
+          <div className="sidebar">
+            <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+              <div className="image">
+                <img
+                  src="dist/img/user2-160x160.jpg"
+                  className="img-circle elevation-2"
+                  alt="User Imasge"
+                />
+              </div>
+              <div className="info">
+                <a href="#!" className="d-block">
+                  Alexander Pierce
+                </a>
+              </div>
+            </div>
+            <div className="form-inline">
+              <div className="input-group" data-widget="sidebar-search">
+                <input
+                  className="form-control form-control-sidebar"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <div className="input-group-append">
+                  <button className="btn btn-sidebar">
+                    <i className="fas fa-search fa-fw" />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <nav className="mt-2">
+              <ul
+                className="nav nav-pills nav-sidebar flex-column"
+                data-widget="treeview"
+                role="menu"
+                data-accordion="false"
+              >
+                <li className="nav-item menu-open">
+                  <a href="#!" className="nav-link active">
+                    <i className="nav-icon fas fa-tachometer-alt" />
+                    <p>Dashboard</p>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#!" className="nav-link">
+                    <i className="nav-icon fas fa-table" />
+                    <p>
+                      Tables
+                      <i className="fas fa-angle-left right" />
+                    </p>
+                  </a>
+                  <ul className="nav nav-treeview">
+                    <li className="nav-item">
+                      <a href="pages/tables/data.html" className="nav-link">
+                        <i className="far fa-circle nav-icon" />
+                        <p>DataTables</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </aside>
+        <div className="content-wrapper" />
+        <aside className="control-sidebar control-sidebar-dark" />
+      </div>
+    </>
   );
 };
+
 export default Dashboard;
